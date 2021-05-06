3566be158002917f2d2dd852cffe21fc9a4b69e7
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-06 16:01:12 +0300

    isItANum

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ import { postsArr } from "./posts.js";
 const postContainerEl = document.querySelector(".posts-container");
 
 console.log(postsArr);
+
 let htmlArticleString = "";
+
 postsArr.forEach((post) => {
   htmlArticleString += `
     <article>
         <h3>${makeTextElipsis(post.title, 4)}</h3>
         <p>${makeTextElipsis(post.body, 7)}</p>
-        <button>Read more</button>
+        <button>Read more</button> 
+        <span>userId: ${post.userId}</span>
     </article>
     `;
 });
@@ -21,3 +24,14 @@ postContainerEl.innerHTML = htmlArticleString;
 function makeTextElipsis(text, howManyWords = 5) {
   return text.split(" ").slice(0, howManyWords).join(" ") + "...";
 }
+
+function isItANumber(dalykas) {
+  if (!isNaN(+dalykas)) {
+    console.log("skaicius");
+  } else {
+    console.log("neskaicius");
+  }
+}
+
+isItANumber("kazkas");
+isItANumber("    15  ");
