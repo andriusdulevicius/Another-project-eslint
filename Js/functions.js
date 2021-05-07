f5df41de40e2cdb51c0e0a8751d2b9778a51a872
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-07 14:02:18 +0300

    properFN

--- Js/functions.js
+++ Js/functions.js
@@ -16,3 +16,23 @@ export function generatePosts(whereToPut) {
   });
   whereToPut.innerHTML = htmlArticleString;
 }
+
+export function generatePostElements(whereToPut) {
+  postsArr.forEach((post) => {
+    const newArticle = document.createElement("article");
+
+    const newTitle = document.createElement("h3");
+    newTitle.textContent = makeTextElipsis(post.title, 4);
+
+    const p = document.createElement("p");
+    p.textContent = makeTextElipsis(post.body, 7);
+
+    const newButton = document.createElement("button");
+    newButton.className = "open-modal";
+    newButton.textContent = "Read more";
+    // newButton.addEventListener("click", toggleModal);
+
+    newArticle.append(newTitle, p, newButton);
+    whereToPut.appendChild(newArticle);
+  });
+}
--- app.js
+++ app.js
@@ -2,20 +2,24 @@
 
 import { postsArr } from "./Js/posts.js";
 import { makeTextElipsis } from "./Js/helperFunc.js";
-import { generatePosts } from "./Js/functions.js";
+import { generatePosts, generatePostElements } from "./Js/functions.js";
 
 const postContainerEl = document.querySelector(".posts-container");
 
 console.log(postsArr);
 
 generatePosts(postContainerEl);
+// generatePostElements(postContainerEl);
 
 // uzdeti event listeneri ant viso posts el, ir klausyti kada paspaudem ant m
 // mygtuko
 postContainerEl.addEventListener("click", function (event) {
   console.log("paspadem");
+  console.log(event.target);
 
   // pasitikrinti ar paspaudem ant mygtuko
 
   // uzeti klase modalui open arba nuimti jei tokia yra
+
+  // uzpildyti modala su duomenimis is to posto ant kurio paspausta
 });
